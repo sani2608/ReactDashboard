44c2f59a3eef41e5760153f6986be000772c33d9
sani2608 <sanikumarsahani@gmail.com>
2022-06-09 23:51:14 +0530

    routes added with components

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Calender = () => {
-  return <div>Calender</div>;
+const Calendar = () => {
+  return <div>Calendar</div>;
 };
 
-export default Calender;
+export default Calendar;
